923fc854a42b386fe2947b8320bb0408fe1062b8
杨哲 <2850755733@qq.com>
2019-06-16 18:01:53 +0800

    新增基础组件的docker部署方式

--- zuul/src/main/java/com/bingo/spring/zuul/ZuulApplication.java
+++ zuul/src/main/java/com/bingo/spring/zuul/ZuulApplication.java
@@ -2,8 +2,10 @@ package com.bingo.spring.zuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulServer;
 
 @SpringBootApplication
+@EnableZuulServer
 public class ZuulApplication {
 
     public static void main(String[] args) {
