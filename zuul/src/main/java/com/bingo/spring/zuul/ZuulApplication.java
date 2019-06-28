8414aebf405f98de6159ca899179ce2bca30cfb8
杨哲 <2850755733@qq.com>
2019-06-28 16:16:06 +0800

    修改网关启动注解

--- zuul/src/main/java/com/bingo/spring/zuul/ZuulApplication.java
+++ zuul/src/main/java/com/bingo/spring/zuul/ZuulApplication.java
@@ -2,10 +2,11 @@ package com.bingo.spring.zuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.cloud.netflix.zuul.EnableZuulServer;
 
 @SpringBootApplication
-@EnableZuulServer
+@EnableZuulProxy //路由注册
 public class ZuulApplication {
 
     public static void main(String[] args) {
